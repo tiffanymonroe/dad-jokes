31216f86a5a2f7598882c2359cdc501910a43ff8
Tiffany Monroe <13427358+tiffanymonroe@users.noreply.github.com>
2022-06-21 06:58:07 -0700

    api

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ const dadImage = document.querySelector(".dad-image");
 const joke = document.querySelector(".joke");
 
 const button = document.querySelector("button");
+
+const getJoke = async function () {
+    const url = "https://icanhazdadjoke.com/";
+    const options = {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    };
+
+    const request = await fetch(url, options);
+    console.log(request);
+    const response = await request.json();
+
+    joke.innerText = response.joke;
+
+};
+
+button.addEventListener("click", function(){
+  getJoke();
+});
